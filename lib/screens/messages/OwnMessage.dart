78ecf336aede0ff585fa154019be9a55d6894328
Enes Ugan <enesyavuz.ug@gmail.com>
2022-06-24 16:21:07 +0200

    add: double tap on message to add to clipboard

--- lib/screens/messages/OwnMessage.dart
+++ lib/screens/messages/OwnMessage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 class OwnMessageCart extends StatelessWidget {
   String message;
@@ -13,44 +14,57 @@ class OwnMessageCart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.centerRight,
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          maxWidth: MediaQuery.of(context).size.width - 45,
-        ),
-        child: Card(
-          elevation: 1,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(11)),
-          color: Colors.blueAccent,
-          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 10, right: 60, top: 5, bottom: 20),
-                child: SelectableText(
-                  this.message,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
+      child: GestureDetector(
+        onDoubleTap: () {
+          //_pushAddedToClippboard(context);
+          Clipboard.setData(ClipboardData(text: this.message));
+        },
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxWidth: MediaQuery.of(context).size.width - 45,
+          ),
+          child: Card(
+            elevation: 1,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(11)),
+            color: Colors.blueAccent,
+            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 10, right: 60, top: 5, bottom: 20),
+                  child: SelectableText(
+                    this.message,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              Positioned(
-                bottom: 4,
-                right: 10,
-                child: Text(
-                  this.time,
-                  style: TextStyle(
-                    fontSize: 13,
-                    color: Colors.white,
+                Positioned(
+                  bottom: 4,
+                  right: 10,
+                  child: Text(
+                    this.time,
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _pushAddedToClippboard(BuildContext context) {
+    Clipboard.setData(new ClipboardData(text: this.message)).then((_) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text('Copied to your clipboard !')));
+    });
   }
 }
